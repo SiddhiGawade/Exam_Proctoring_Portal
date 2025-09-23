6cc092e5075971833fc90c2016e9f0486c300ddd
TANISHQ <tanishqkulkarni18@gmail.com>
2025-09-23 11:54:21 +0530

    Update server port

--- app.py
+++ app.py
@@ -674,5 +674,5 @@ def handle_stop_ricart():
 # --- MAIN EXECUTION BLOCK ---
 if __name__ == '__main__':
     processor_server = ProcessorServer()
-    print("Website running on http://127.0.0.1:5000")
-    socketio.run(app, host='127.0.0.1', port=5000, debug=False, allow_unsafe_werkzeug=True)
+    print(f"Website running on http://127.0.0.1:{os.environ.get('PORT', 8080)}")
+    socketio.run(app, host='127.0.0.1', port=int(os.environ.get('PORT', 8080)), debug=False, allow_unsafe_werkzeug=True)
